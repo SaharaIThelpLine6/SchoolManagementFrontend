6bf6a424ec5e9a0f9b1e1a41382af76d736de409
SaharaIThelpLine6 <saharaitpc1@gmail.com>
2025-01-02 13:22:00 +0600

    require component added in layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -5,6 +5,8 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import { useSelector } from 'react-redux';
+import Header from "../components/Header";
+import Filter from "../components/Filter";
 
 const menuData = [
   {
