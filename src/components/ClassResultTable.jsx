d58a75da1eba189aa807f9cea1ce9394da362b2a
SaharaHelpline-7 <jonaidulkhan@gmail.com>
2025-01-25 11:49:52 +0600

    class result, classwise marksheet

---
+++ src/components/ClassResultTable.jsx
@@ -0,0 +1,119 @@
+const ClassResultTable = () => {
+
+
+    return (
+        <div className="">
+            <div className=" mx-auto relative bg-white font-SolaimanLipi">
+                <div className=" pt-0 pb-1 px-2 bg-white">
+                    <div className="bg-[#307847]">
+                        {/*Logo*/}
+                        <div className="absolute pt-5 pl-5">
+                            <img src="/logoremovebg.png" alt="logo" width={100} height={100} className="" />
+                        </div>
+                       
+                        {/*Heading Title Start*/}
+                        <div className="mx-auto text-center py-3 text-white">
+                            <h2 className="text-[22px] font-medium">জামিয়াতুস সুন্নাহ লিলবানাত বসুরহাট (বসুরহাট মহিলা মাদরাসা)</h2>
+                            <h3 className="">কোম্পানিগঞ্জ, নোয়াখালী</h3>
+                            <h3 className="">২য় সাময়িক পরীক্ষা - ২০২৪ইং</h3>
+                            <h3 className="border-2 border-white rounded-md p-1 w-fit mx-auto">ফলাফল(নম্বরপত্র)</h3>
+                            <h3 className="">শ্রেনী/জামাত : প্রথম শ্রেণী</h3>
+                        </div>
+                    </div>
+                    {/*Heading Title End*/}
+                    {/*Marksheet Body Start*/}
+                    <div className="pt-2 w-full overflow-x-scroll ">
+                        <table className="w-fit text-center text-[16px]">
+                            <thead className="border border-black">
+                                <tr>
+                                    <td className="border border-black px-1 min-w-[35px]">ক্র:</td>
+                                    <td className="border border-black min-w-[70px]">আইডি নং</td>
+                                    <td className="border border-black min-w-[180px]">শিক্ষার্থীর নাম</td>
+                                    <td className="border border-black px-1 min-w-[100px] ">কালিমা</td>
+                                    <td className="border border-black px-1 min-w-[100px]">ইংরেজী</td>
+                                    <td className="border border-black px-1 min-w-[100px]">বাংলা</td>
+                                    <td className="border border-black px-1 min-w-[100px]">গণিত </td>
+                                    <td className="border border-black px-1 min-w-[100px]">মাসআলা ও দোয়া</td>
+                                    <td className="border border-black px-1 min-w-[100px]">হাদীস</td>
+                                    <td className="border border-black px-1 min-w-[100px]">আরবী লিখা</td>
+                                    <td className="border border-black px-1 min-w-[100px]">আদব আখলাক</td>
+                                    <td className="border border-black px-1 min-w-[100px]">কালিমা</td>
+                                    <td className="border border-black px-1 min-w-[100px]">ইংরেজী</td>
+                                    <td className="border border-black px-1 min-w-[100px]">বাংলা</td>
+                                    <td className="border border-black px-1 min-w-[100px]">গণিত </td>
+                                    <td className="border border-black px-1 min-w-[100px]">মাসআলা ও দোয়া</td>
+                                    <td className="border border-black px-1 min-w-[100px]">হাদীস</td>
+                                    <td className="border border-black px-1 min-w-[100px]">আরবী লিখা</td>
+                                    <td className="border border-black px-1 min-w-[100px]">আদব আখলাক</td>
+                                    <td className="border border-black px-1 min-w-[100px]">আদব আখলাক</td>
+                                    <td className="border border-black px-1 min-w-[50px]">মোট</td>
+                                    <td className="border border-black px-1 min-w-[60px]">গড়</td>
+                                    <td className="border border-black px-1 min-w-[60px]">বিভাগ</td>
+                                    <td className="border border-black px-1 min-w-[20px]">স্থান</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="border border-black">১</td>
+                                    <td className="border border-black">৪১৭১৩</td>
+                                    <td className="border border-black pl-1 text-left">খাজিদা আক্তার সাজিয়া</td>
+                                    <td className="border border-black">৯৮</td>
+                                    <td className="border border-black">৯৭</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">৯৩</td>
+                                    <td className="border border-black">৯৫</td>
+                                    <td className="border border-black">৯৮</td>
+                                    <td className="border border-black">৯৭</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">৯৩</td>
+                                    <td className="border border-black">৯৫</td>
+                                    <td className="border border-black">৯৫</td>
+                                    <td className="border border-black">৭৬৭</td>
+                                    <td className="border border-black">৯৫.৮৮</td>
+                                    <td className="border border-black">মুমতায</td>
+                                    <td className="border border-black">১</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-black">২</td>
+                                    <td className="border border-black">৪১৭১৩</td>
+                                    <td className="border border-black text-left pl-1">খাজিদা আক্তার সাজিয়া</td>
+                                    <td className="border border-black">৯৮</td>
+                                    <td className="border border-black">৯৭</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">৯৩</td>
+                                    <td className="border border-black">৯৫</td>
+                                    <td className="border border-black">৯৮</td>
+                                    <td className="border border-black">৯৭</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">৯২</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">১০০</td>
+                                    <td className="border border-black">৯৩</td>
+                                    <td className="border border-black">৯৫</td>
+                                    <td className="border border-black">৯৫</td>
+                                    <td className="border border-black">৭৬৭</td>
+                                    <td className="border border-black">৯৫.৮৮</td>
+                                    <td className="border border-black">মুমতায</td>
+                                    <td className="border border-black">১৯</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    {/*Marksheet Body End*/}
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ClassResultTable;
