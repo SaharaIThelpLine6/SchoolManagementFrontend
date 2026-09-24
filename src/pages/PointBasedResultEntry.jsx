245c9190ffa491ef296a49347a4ecf50a9262011
azam <mdkaydaazam1@gmail.com>
2026-09-24 11:55:48 +0600

    Update point based result entry

--- src/pages/PointBasedResultEntry.jsx
+++ src/pages/PointBasedResultEntry.jsx
@@ -162,11 +162,16 @@ const PointBasedResultEntry = ({ pageTitle }) => {
   };
 
 
-  const handleTeacherAssign = async () => {
-    navigate(
-      `/dashboard/result/teacher_asign_subject`
-    )
-  }
+  const handleTeacherAssign = () => {
+    showModal('শিক্ষক বিষয় বরাদ্দ', 'HANDLE_RESULT_ENTRY_ASSIGN', {
+      closeOnOutSide: false,
+    });
+  };
+  // const handleTeacherAssign = async () => {
+  //   navigate(
+  //     `/dashboard/result/teacher_asign_subject`
+  //   )
+  // }
   // Table Data Columns 
   const columns = [
     {
@@ -272,8 +277,27 @@ const PointBasedResultEntry = ({ pageTitle }) => {
         <h3 className="text-base sm:text-[20px] font-bold">
           {translate('Point Result Entry')}
         </h3>{' '}
-        <Button className='' onClick={() => { handleTeacherAssign() }}>
+        {/* <Button className='' onClick={() => { handleTeacherAssign() }}>
           শিক্ষক-বিষয় গ্রুপ
+        </Button> */}
+        <Button onClick={handleTeacherAssign}>
+          <span className="flex items-center gap-1.5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={18}
+              height={18}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M12 5v14" />
+              <path d="M5 12h14" />
+            </svg>
+            শিক্ষক বিষয় বরাদ্দ
+          </span>
         </Button>
       </div>
       <FormProvider {...methods}>
